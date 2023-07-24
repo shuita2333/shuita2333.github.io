de2d8f4347d286b91692bda36e8d7656487983c0
zyh <603747072@qq.com>
2023-07-24 19:20:28 +0800

    Site updated: 2023-07-24 19:20:28

--- js/title.js
+++ js/title.js
@@ -4,14 +4,14 @@ var titleTime;
 document.addEventListener('visibilitychange', function () {
   if (document.hidden) {
     //离开当前页面时标签显示内容
-    document.title = '👀我在这 我在这~';
-    clearTimeout(titleTime);
+    // document.title = '👀我在这 我在这~';
+    // clearTimeout(titleTime);
   } else {
     //返回当前页面时标签显示内容
-    document.title = '抓到你啦～';
+    document.title = '👀欢迎回来～';
     //两秒后变回正常标题
     titleTime = setTimeout(function () {
       document.title = OriginTitile;
-    }, 1000);
+    }, 2000);
   }
 });
