3f41315c36b5c4844f05ec3b1891ec589d25e0ab
zyh <603747072@qq.com>
2023-07-29 18:56:37 +0800

    Site updated: 2023-07-29 18:56:37

--- js/another.js
+++ js/another.js
@@ -1,26 +1,26 @@
-// //随机文章推荐
-// // 发现有时会和当前页面重复，加一个判断  
-// function randomPost() {
-//     fetch('/baidusitemap.xml').then(res => res.text()).then(str => (new window.DOMParser()).parseFromString(str, "text/xml")).then(data => {
-//         let ls = data.querySelectorAll('url loc');
-//         while (true) {
-//             let url = ls[Math.floor(Math.random() * ls.length)].innerHTML;
-//             if (location.href == url) continue;
-//             location.href = url;
-//             return;
-//         }
-//     })
-// }
+//随机文章推荐
+// 发现有时会和当前页面重复，加一个判断  
+function randomPost() {
+    fetch('/baidusitemap.xml').then(res => res.text()).then(str => (new window.DOMParser()).parseFromString(str, "text/xml")).then(data => {
+        let ls = data.querySelectorAll('url loc');
+        while (true) {
+            let url = ls[Math.floor(Math.random() * ls.length)].innerHTML;
+            if (location.href == url) continue;
+            location.href = url;
+            return;
+        }
+    })
+}
 
 
-hexo.extend.generator.register('random', function (locals) {
-    const config = hexo.config.random || {}
-    const posts = []
-    for (const post of locals.posts.data) {
-        if (post.random !== false) posts.push(post.path)
-    }
-    return {
-        path: config.path || 'zhheo/random.js',
-        data: `var posts=${JSON.stringify(posts)};function toRandomPost(){window.open('/'+posts[Math.floor(Math.random() * posts.length)],"_self");};`
-    }
-})
+// hexo.extend.generator.register('random', function (locals) {
+//     const config = hexo.config.random || {}
+//     const posts = []
+//     for (const post of locals.posts.data) {
+//         if (post.random !== false) posts.push(post.path)
+//     }
+//     return {
+//         path: config.path || 'zhheo/random.js',
+//         data: `var posts=${JSON.stringify(posts)};function toRandomPost(){window.open('/'+posts[Math.floor(Math.random() * posts.length)],"_self");};`
+//     }
+// })
